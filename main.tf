47716e4469be9acd49bf3f7006541acc7bd98590
abhishek <abhishekpandey8123@gmail.com>
2025-05-07 17:01:14 +0530

    hook

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-062f0cc54dbfd8ef1"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "any change it will run"
+    }
+  
+}
